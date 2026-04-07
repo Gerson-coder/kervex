f296ca7fbd6ee21c57c7bb4676936b0f584ef64d
Gerson <gersonherrerach@gmail.com>
2026-04-07 09:15:12 -0500

    fix: update site domain from verkex.pe to verkex.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://verkex.pe',
+  site: 'https://verkex.com',
   output: 'static',
   prefetch: true,
   integrations: [sitemap()],
